0170fbc312e7cebdb63ec1e19904c9c1c0ef14ca
Adelin <aa.gruian@gmail.com>
2022-10-12 19:49:12 +0300

    v2.1 - MyNotes -Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MyNotes',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Adelin Gruian',
+    author_email='',
+    description=''
+)
